52336aabe9b02ad16d0251ed30b0a6312ba4b50e
@tammka <alekssannikova@gmail.com>
2024-04-25 15:01:48 +0300

    hometask2

---
+++ variables.py
@@ -0,0 +1,9 @@
+employee_name = 'Alex'
+employee_age = 35
+employee_salary = 831.37
+employee_marriage_status = False
+
+print('employee_name type:', type(employee_name))
+print('employee_age type:', type(employee_age))
+print('employee_salary type:', type(employee_salary))
+print('employee_marriage_status type:', type(employee_marriage_status))
